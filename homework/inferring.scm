ca601333fd3099ea1b12d76a1fa7b850cd7d3b1b
Chris Wright <cawright@scu.edu>
2018-06-03 10:24:20 -0700

    initialized finals for final project

--- homework/inferring.scm
+++ homework/inferring.scm
@@ -11,20 +11,37 @@
 	(cond 	((atom? element) (list 'NOT element))
 		(#t (cadr element))))
 
-(define (resolve c1 c2) 
+(define (resolve c1 c2)
+	(if (can-unify? c1 c2)
+	(let ((ans (recurse-c1 c1 c2))) 
+		(cond 	((null? ans) 'CONTRADICTION)
+			(#t ans))) 
+	#f))
+
+(define (recurse-c1 c1 c2) 
 	(cond 	((null? c1) #f)
-		(#t (let ((ans (resolve-helper c1 c2))) 
-			(if ans ans (let ((ans2 (resolve (cdr c1) c2))) 
+		(#t (let ((ans (recurse-c2 c1 c2))) 
+			(if ans ans (let ((ans2 (recurse-c1 (cdr c1) c2))) 
 				(if ans2 (cons (car c1) ans2) #f))))))) 	
 
 ; resolve helper takes in two lists, c1 is guranteed to have at least 1 element
 ; if can unify first element of c1, with any element of c2, returns unified list
 ; otherwise returns false.
-(define (resolve-helper c1 c2)
+(define (recurse-c2 c1 c2)
 	(cond 	((null? c2) #f)
 		((equal? (NOT (car c1)) (car c2)) (append (cdr c1) (cdr c2)))
-		(#t (let ((ans (resolve-helper c1 (cdr c2)))) 
+		(#t (let ((ans (recurse-c2 c1 (cdr c2)))) 
 			(if ans (cons (car c2) ans) #f)))))
+(define (can-unify? c1 c2)
+	(if (and (element-in-common c1 c2) (distinct-element c1 c2)) #t #f))
+
+(define (element-in-common c1 c2)
+	(cond 	((null? c1) #f)
+		(#t (if (contains? c2 (NOT (car c1))) #t (element-in-common (cdr c1) c2)))))
+
+(define (distinct-element c1 c2)
+	(cond 	((null? c1) #f)
+		(#t (if	(contains? c2 (NOT (car c1))) (distinct-element (cdr c1) c2) #t))))
 
 ;pre: none
 ;post: returns true if lst contains item
@@ -39,8 +56,5 @@
 ; KB is a list, each element in list is statement in CNF form
 (define KB '())
 
-(define (tell statement) 
-	(if (tell-helper statement) 'OK 'ERROR))
-
-(define (tell-helper statement)
-	(define KB (cons statement KB))) 
+(define (tell statement)
+	(set! KB (cons statement KB))) 
